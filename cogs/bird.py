07fa8382c3f119eb93c85f6cbeba02276fdda5e7
Brett Bender <brett@brettbender.me>
2019-09-29 20:57:19 -0500

    Updated chat formatting

--- cogs/bird.py
+++ cogs/bird.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from cogs.utils import permissions
+from cogs.utils import chat_formatting
 import subprocess
-
 
 class Bird:
     """Do things with BIRD"""
@@ -13,30 +13,34 @@ class Bird:
     async def route(self, subnet):
         """Enter an IP or subnet to lookup in BIRD"""
         cmd = subprocess.check_output(["birdc", "show", "route", "for", str(subnet)])
-        out = f"```\n{cmd}\n```"
-        await self.bot.say(out)
+        out = "```\n{0}\n```".format(cmd)
+        for page in chat_formatting.pagify(out, ['\n', ' '], shorten_by=12):
+            await self.bot.say(page)
 
     @commands.command()
     async def status(self):
         """Check the status of BIRD"""
         cmd = subprocess.check_output(["birdc", "show", "proto"])
-        out = f"```\n{cmd}\n```"
-        await self.bot.say(out)
+        out = "```\n{0}\n```".format(cmd)
+        for page in chat_formatting.pagify(out, ['\n', ' '], shorten_by=12):
+            await self.bot.say(page)
 
     @commands.command()
     async def statusinfo(self, astable):
         """Enter an IP or subnet to lookup in BIRD"""
         cmd = subprocess.check_output(["birdc", "show", "proto", str(astable)])
-        out = f"```\n{cmd}\n```"
-        await self.bot.say(out)
+        out = "```\n{0}\n```".format(cmd)
+        for page in chat_formatting.pagify(out, ['\n', ' '], shorten_by=12):
+            await self.bot.say(page)
 
     @commands.command()
     @commands.check(permissions.admincheck)
     async def birdrestart(self, astable):
         """Enter an IP or subnet to restart in BIRD"""
         cmd = subprocess.check_output(["birdc", "restart", str(astable)])
-        out = f"```\n{cmd}\n```"
-        await self.bot.say(out)
+        out = "```\n{0}\n```".format(cmd)
+        for page in chat_formatting.pagify(out, ['\n', ' '], shorten_by=12):
+            await self.bot.say(page)
 
 
 def setup(bot):
