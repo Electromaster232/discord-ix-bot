7c7da044e92365945dab8adbb443ca746fa134e4
Electromaster232 <gilbert7573@gmail.com>
2019-09-29 21:27:55 -0400

    bird ready!

--- cogs/bird.py
+++ cogs/bird.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
-import os
+from cogs.utils import permissions
+import subprocess
+
 
 class Bird:
     """Do things with BIRD"""
@@ -9,9 +11,32 @@ class Bird:
 
     @commands.command()
     async def route(self, subnet):
-        """Enter an IP or subnet to lookup"""
+        """Enter an IP or subnet to lookup in BIRD"""
+        cmd = subprocess.check_output(["birdc", "show", "route", "for", str(subnet)])
+        out = f"```\n{cmd}\n```"
+        await self.bot.say(out)
 
+    @commands.command()
+    async def status(self):
+        """Check the status of BIRD"""
+        cmd = subprocess.check_output(["birdc", "show", "proto"])
+        out = f"```\n{cmd}\n```"
+        await self.bot.say(out)
 
+    @commands.command()
+    async def statusinfo(self, astable):
+        """Enter an IP or subnet to lookup in BIRD"""
+        cmd = subprocess.check_output(["birdc", "show", "proto", str(astable)])
+        out = f"```\n{cmd}\n```"
+        await self.bot.say(out)
+
+    @commands.command()
+    @commands.check(permissions.admincheck)
+    async def birdrestart(self, astable):
+        """Enter an IP or subnet to restart in BIRD"""
+        cmd = subprocess.check_output(["birdc", "restart", str(astable)])
+        out = f"```\n{cmd}\n```"
+        await self.bot.say(out)
 
 
 def setup(bot):
